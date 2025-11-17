5f1ae6fb47974ee0b7de5ba8a09a06505ddcbda9
Mariana <mlete@proton.me>
2025-11-17 14:42:12 +0100

    adds UpdateAccountForm, with user, email and profile picture update

--- event_finder/forms.py
+++ event_finder/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField #wtforms is a package
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError #DataRequired is a class we are importing
+from flask_wtf.file import FileField, FileAllowed
 from event_finder.models import User
 
 
@@ -30,3 +32,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField ('Log in')
+
+class UpdateAccountForm(FlaskForm):
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField ('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That email is taken. Please choose another one.')
